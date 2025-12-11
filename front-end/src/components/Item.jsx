8fe95a3cc9ff948b40611845bb91932c546b1c8e
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-12-11 01:08:49 -0300

    feat: melhoria nos items de acomodacao

--- front-end/src/components/Item.jsx
+++ front-end/src/components/Item.jsx
@@ -3,6 +3,14 @@ import image1 from "../assets/lugares/image__1.jpg";
 import { Skeleton } from "@mantine/core";
 import MarkdownIt from "markdown-it";
 
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+
 const Item = ({ place = null, placeHolder }) => {
 	const md = new MarkdownIt({
 		html: false,
@@ -24,12 +32,32 @@ const Item = ({ place = null, placeHolder }) => {
 				</div>
 			) : (
 				<Link to={`/places/${place._id}`}>
-					<div className="flex flex-col gap-2 group rounded-2xl hover:p-3 hover:bg-primary-100  transition-all duration-700 max-w-[350px]">
-						<img
-							src={place.photos[0]}
-							alt="Imagem da acomodação"
-							className="aspect-square object-cover  transition-transform rounded-2xl"
-						/>{" "}
+					<div className="flex flex-col gap-2 group rounded-2xl hover:p-3 hover:bg-primary-100 transition-all duration-700 max-w-[350px]">
+						<Carousel
+							opts={{
+								loop: true,
+							}}
+							className="w-full max-w-xs z-99 relative"
+						>
+							<CarouselContent>
+								{place.photos.map((_, index) => (
+									<CarouselItem key={index}>
+										<img
+											src={place.photos[index]}
+											alt="Imagem da acomodação"
+											className="aspect-square object-cover transition-transform rounded-2xl"
+										/>
+									</CarouselItem>
+								))}
+							</CarouselContent>
+							<div onClick={(e) => e.preventDefault()}>
+								<CarouselPrevious className="absolute left-2 text-white bg-transparent hover:cursor-pointer " />
+							</div>
+							<div onClick={(e) => e.preventDefault()}>
+								<CarouselNext className="absolute right-2 bg-transparent text-white hover:cursor-pointer" />
+							</div>
+						</Carousel>
+
 						<div className="">
 							<h3 className="text-xl font-semibold line-clamp-1">
 								{place.city}
