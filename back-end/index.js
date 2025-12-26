7e40f61f18d85322795f95b243dcdc11cdf8c7b6
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-12-26 02:20:47 -0300

    feat: Preparando deploy

--- back-end/index.js
+++ back-end/index.js
@@ -2,13 +2,19 @@ import "dotenv/config";
 import { app } from "./server.js";
 import { connectDb } from "./config/db.js";
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-  await connectDb();
-  app.listen(3000, "0.0.0.0", () => {
-  console.log("Servidor rodando em http://0.0.0.0:3000");
-});
+  try {
+    await connectDb();
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Erro ao iniciar o servidor:", error);
+    process.exit(1);
+  }
 };
 
 startServer();
